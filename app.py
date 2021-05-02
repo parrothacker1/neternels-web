2712252b05f95e8e95af8fc806867581af182927
NetErnels <80594207+Neternels@users.noreply.github.com>
2021-05-03 01:40:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+from modules.connector import SQL
+import base64,os
 from flask import Flask,render_template
+from flask_heroku import Heroku
+from flask import request as req
+from flask_mobility import Mobility
 app=Flask(__name__)
+Mobility(app)
+Heroku(app)
+sql=SQL()
 @app.route('/')
-def hello():
-	return render_template('index.html')
-
+def index():
+        return render_template('index.html',name=sql.get_members(),len1=len(sql.get_mem>
 if __name__=="__main__":
 	app.run()
