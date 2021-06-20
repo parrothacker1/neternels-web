7eaa2079bfc476a9e97fdec88136bb43c711fcd0
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2021-06-20 23:41:45 +0530

    Fixing fronted part 1

--- app.py
+++ app.py
@@ -145,5 +145,5 @@ def not_found(e):
 if __name__=='__main__':
 	app.config['TEMPLATES_AUTO_RELOAD'] = True
 	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-	app.run(debug=True,threaded=True)
+	app.run(host='0.0.0.0',debug=True,threaded=True)
 
